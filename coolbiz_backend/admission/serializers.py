5c29a2740afef4c9f3c9d6b23b6b13a173c0e9a0
Matt Shirley <matt.shirley@t-3.com>
2018-12-30 20:30:38 -0600

    allow username in AdmissionSerializer.create

--- coolbiz_backend/admission/serializers.py
+++ coolbiz_backend/admission/serializers.py
@@ -6,4 +6,4 @@ class AdmissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Admission
         fields = ('id', 'username', 'first_name', 'last_name',)
-        read_only_fields = ('username', )
+        #read_only_fields = ('username', )
